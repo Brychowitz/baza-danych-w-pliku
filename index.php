04a65bba6b7c80259b71b9a2585d8e5ef4b456a0
Przemysław Brych <przemek.brych123@gmail.com>
2019-11-15 15:40:34 +0100

    Poprawki
    Poprawne wypisanie danych z pliku pod pierwszym wierszem.

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <!DOCTYPE html>
-<html>
-	<body>
-		<?php
-			$mojplik = @fopen("baza.txt", "r") or die ("Unable to open file!");
-			
-			while(!feof($mojplik)) {
-				echo fgets($mojplik). "<br>";
-				}
-				fclose($mojplik);
-		?>
-	</body>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            $mojplik = @fopen("baza.txt", "r") or die ("Błąd pliku!");
+            $tablica = array();
+            while( !feof($mojplik)){
+                    $tablica[] = fgets($mojplik);
+            }
+            fclose($mojplik);
+        ?>
+        <table>
+			<tr>
+				<th>Id</th>
+				<th>Imie</th>
+				<th>Nazwisko</th>
+				<th>Zawod</th>
+				<th>Pensja</th>
+			</tr>
+            <?php
+                foreach($tablica as $wiersz){
+                    $rekord = explode(" ", $wiersz);
+					echo '<tr>';
+					echo "<td>$rekord[0]</td>";
+					echo "<td>$rekord[1]</td>";
+					echo "<td>$rekord[2]</td>";
+					echo "<td>$rekord[3]</td>";
+					echo "<td>$rekord[4]</td>";
+					echo "</tr>";
+                }
+            ?>
+        </table>
+    </body>
 </html>
